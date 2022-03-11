a3659dcb08a9a2851f3127bc82bfe2c60530e113
이민형 <leemihzy@gmail.com>
2022-03-11 16:08:08 +0900

    chore: modify contents style

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,6 @@
+const weather = document.querySelector('#weather span:first-child');;
+const city = document.querySelector('#weather span:last-child');;
+
 const API_KEY = "e74eec3bb6d35639807b1f7f6b6b8d83";
 
 function onGeoOk(position) {
@@ -7,8 +10,6 @@ function onGeoOk(position) {
 
   // url 응답 얻기(요청)
   fetch(url).then(response => response.json()).then(data => {
-    const weather = document.querySelector('#weather span:first-child');;
-    const city = document.querySelector('#weather span:last-child');;
     city.innerText = data.name;
     weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
   }) 
